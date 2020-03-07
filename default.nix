e98e32da8e6089494701f259d27ea7276b350fed
Kevin Han <kevinhan@Kevins-MacBook-Air.local>
2020-03-07 15:04:52 +0800

    init: python nix-shell
    Initialize a nix-shell with some most commonly used python packages.

---
+++ default.nix
@@ -0,0 +1,49 @@
+with import <nixpkgs> {};
+stdenv.mkDerivation rec {
+  name = "env";
+
+  # Mandatory boilerplate for buildable env
+  env = buildEnv { name = name; paths = buildInputs; };
+  builder = builtins.toFile "builder.sh" ''
+    source $stdenv/setup; ln -s $env $out
+  '';
+
+  # Customizable development requirements
+  buildInputs = [
+    # Add packages from nix-env -qaP | grep -i needle queries
+    bat
+    curl
+    exa
+    git
+    tmux
+    wget
+
+    # With Python configuration requiring a special wrapper
+    (python37.buildEnv.override {
+      ignoreCollisions = true;
+      extraLibs = with python37Packages; [
+        numpy
+        matplotlib
+        scipy
+        ipython
+        scikitlearn
+        nltk
+        Keras
+        pandas
+        black
+        lightgbm
+        seaborn
+        shapely
+        jupyter
+        jupyterlab
+        pyspark
+      ];
+    })
+
+    # Vim
+  ];
+
+  # Customizable development shell setup with at last SSL certs set
+  shellHook = ''
+  '';
+}
